81c381ce4706c78cdd0a819523515775af2ebc8d
Angel Leger <dark_rule89@hotmail.com>
2025-09-03 18:42:41 -0400

    Add feature tests for response cache middleware
    Implemented feature tests for response caching, ETag handling, and cache invalidation by tags. Added Pest and TestCase setup for consistent test environment using array cache store. Minor whitespace fix in service provider.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,1 +1,22 @@
-<?php // full code from doc ... ?>
+<?php
+
+declare(strict_types=1);
+
+namespace AngelLeger\ResponseCache\Tests;
+
+use AngelLeger\ResponseCache\ResponseCacheServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    protected function getPackageProviders($app): array
+    {
+        return [ResponseCacheServiceProvider::class];
+    }
+
+    protected function defineEnvironment($app): void
+    {
+        // Use array cache store during tests
+        $app['config']->set('cache.default', 'array');
+    }
+}
